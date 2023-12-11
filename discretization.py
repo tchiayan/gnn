c90c5b2408d27a6ba15578830ec7680af2678c56
Tan Chia  Yan <tchiayan@gmail.com>
2023-12-11 17:25:34 +0800

    Update discretization

--- discretization.py
+++ discretization.py
@@ -4,6 +4,7 @@ import os
 from fpgrowth import FPTree
 import itertools
 import sys 
+from mlxtend.frequent_patterns import fpgrowth , association_rules
 
 sys.setrecursionlimit(5000)
 base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "BRCA")
@@ -14,7 +15,7 @@ data = [
     {"field1": 0.9}
 ]
 
-df = pd.read_csv(os.path.join(base_path , "1_tr.csv"))
+df = pd.read_csv(os.path.join(base_path , "1_tr.csv"), header=None)
 est = preprocessing.KBinsDiscretizer(n_bins=2 , encode='ordinal' , strategy='uniform')
 est.fit(df)
 
@@ -24,41 +25,42 @@ df = pd.DataFrame(est.transform(df))
 df_label = pd.read_csv(os.path.join(base_path , "labels_tr.csv"), names=['class'])
 df = df.join(df_label)
 
-for column in df.columns: 
-    df[column] = df[column].astype(str)
-    df[column] = "Feature " + str(column) + " : " + df[column]
+# for column in df.columns: 
+#     df[column] = df[column].astype(str)
+#     df[column] = "Feature " + str(column) + " : " + df[column]
 print(df.head())
-
-
+print(df.describe())
 class_label = df['class'].unique().tolist()
 print(class_label)
 
-print("Generate FP Tree")
-tree = FPTree(df.values , 200 , None , None)
-patterns = tree.mine_patterns(200) #return dict with key: item-set , value: support score
+frequent_itemsets = fpgrowth(df , min_support=0.5 , use_colnames=True)
+
+# print("Generate FP Tree")
+# tree = FPTree(df.values , 200 , None , None)
+# patterns = tree.mine_patterns(200) #return dict with key: item-set , value: support score
 
 
-print("Generate CARs")
-CARs = {}
-for itemset in patterns.keys():
-    upper_support = patterns[itemset]
+# print("Generate CARs")
+# CARs = {}
+# for itemset in patterns.keys():
+#     upper_support = patterns[itemset]
     
-    for i in range(1 , len(itemset)):
-        # Generate possible combination of itemsets (Subset of frequent itemset is frequet itemset)
-        for antecedent in itertools.combinations(itemset , i):
-            antecedent = tuple(sorted(antecedent))
-            consequent = tuple(sorted(set(itemset) - set(antecedent)))
+#     for i in range(1 , len(itemset)):
+#         # Generate possible combination of itemsets (Subset of frequent itemset is frequet itemset)
+#         for antecedent in itertools.combinations(itemset , i):
+#             antecedent = tuple(sorted(antecedent))
+#             consequent = tuple(sorted(set(itemset) - set(antecedent)))
             
-            # obtaining the rules where only the consequent has the class label
-            if len(consequent) == 1 and consequent[0] in class_label:
-                print("FOund")
-                if antecedent in patterns:
-                    lower_support = patterns[antecedent]
-                    confidence = float(upper_support) / lower_support
-                    support = upper_support/len(df)
+#             # obtaining the rules where only the consequent has the class label
+#             if len(consequent) == 1 and consequent[0] in class_label:
+#                 print("FOund")
+#                 if antecedent in patterns:
+#                     lower_support = patterns[antecedent]
+#                     confidence = float(upper_support) / lower_support
+#                     support = upper_support/len(df)
 
-                    # filtering the rules where the confidence of the rules does not satisfy the minimum threshold
-                    if confidence >= 0.3:
-                        CARs[antecedent] = (consequent, confidence, support)
+#                     # filtering the rules where the confidence of the rules does not satisfy the minimum threshold
+#                     if confidence >= 0.3:
+#                         CARs[antecedent] = (consequent, confidence, support)
 
-print(CARs)
+# print(CARs)
