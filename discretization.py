139db0b1b9930d40a9f28adf626307ff682fad68
Tan Chia  Yan <tchiayan@gmail.com>
2023-12-11 19:02:33 +0800

    Fix AC

--- discretization.py
+++ discretization.py
@@ -24,32 +24,37 @@ df = pd.DataFrame(est.transform(df))
 # df.replace({1: True , 0: False} , inplace=True)
 
 df_label = pd.read_csv(os.path.join(base_path , "labels_tr.csv"), names=['class'])
-df = df.join(df_label)
+# df = df.join(df_label)
 
 # for column in df.columns: 
 #     df[column] = df[column].astype(str)
 #     df[column] = "Feature " + str(column) + " : " + df[column]
 # print(df.head())
 # print(df.describe())
-
-class_label = df['class'].unique().tolist()
+df_label['class'] = df_label['class'].astype(str)
+class_label = df_label['class'].unique().tolist()
 print(f"Unique classes: {class_label}")
 
 print("Build transaction")
 transactions = []
-for idx , row in df.iloc[: , 900:].iterrows():
+for idx , row in df.iloc.iterrows():
     transaction = [ f"Feature {idx}" for idx , i in enumerate(row.values) if i == 1 ]
     transactions.append(transaction)
+for idx , row in df_label.iterrows():
+    transactions[idx].append(row[0])
     
 # frequent_itemsets = fpgrowth(df , min_support=0.5 , use_colnames=True)
 
 print("Generate FP Tree")
-tree = FPTree(transactions , 200 , None , None ,)
-patterns = tree.mine_patterns(200) #return dict with key: item-set , value: support score
+tree = FPTree(transactions , 5 , None , None ,)
+print("Mine patterns")
+patterns = tree.mine_patterns(5) #return dict with key: item-set , value: support score
 
 
 print("Generate CARs")
 CARs = {}
+output = []
+    
 for itemset in patterns.keys():
     upper_support = patterns[itemset]
     
@@ -61,7 +66,6 @@ for itemset in patterns.keys():
             
             # obtaining the rules where only the consequent has the class label
             if len(consequent) == 1 and consequent[0] in class_label:
-                print("FOund")
                 if antecedent in patterns:
                     lower_support = patterns[antecedent]
                     confidence = float(upper_support) / lower_support
@@ -70,5 +74,9 @@ for itemset in patterns.keys():
                     # filtering the rules where the confidence of the rules does not satisfy the minimum threshold
                     if confidence >= 0.3:
                         CARs[antecedent] = (consequent, confidence, support)
+                        output.append("\t".join([str(consequent[0]) , str(confidence) , str(support) , ",".join(list(antecedent))]))
 
-print(CARs)
+print(f"Generated CARs length: {len(CARs)}")               
+with open("AC_rules.tsv" , 'w') as ac_file:
+    ac_file.write("\n".join(output))
+
