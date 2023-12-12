dd5afb3b757290554b3ff07aa16f70c303e7920f
Tan Chia  Yan <tchiayan@gmail.com>
2023-12-12 19:01:36 +0800

    Generate CARs with FPGrowth subtree

--- discretization.py
+++ discretization.py
@@ -8,6 +8,7 @@ import argparse
 
 parser = argparse.ArgumentParser("Discretization")
 parser.add_argument("--min_support" , default=100, type=int , help="Min support")
+parser.add_argument("--min_confidence" , default=0.1, type=float , help="Min confidence")
 
 args = parser.parse_args()
 
@@ -39,6 +40,8 @@ df_label['class'] = df_label['class'].astype(str)
 class_labels = df_label['class'].unique().tolist()
 print(f"Unique classes: {class_labels}")
 
+CARs = {}
+output = []
 for label in class_labels:
     print(f"Generate FPTree per class: {label}")
     subdf = df.loc[df_label[df_label['class'] == label].index]
@@ -59,37 +62,47 @@ for label in class_labels:
     tree = FPTree(transactions , args.min_support , None , None ,)
     print("Mine patterns")
     patterns = tree.mine_patterns(args.min_support) #return dict with key: item-set , value: support score
-
-    print(patterns)
+    print(f"Generated frequent item set [Class: {label}]: {len(patterns)}")
     
-    break
+    print("Generate CARs")
+    generated_cars = 0
+    for itemset in patterns.keys():
+        upper_support = patterns[itemset]
+        
+        lower_support = len(patterns)
+        confidence = float(upper_support)/lower_support
+        support = upper_support/len(df)
+        
+        if confidence >= 0.0:
+            generated_cars += 1
+            CARs[itemset] = ( label , confidence , support)
+            output.append("\t".join([str(label) , str(confidence) , str(support) , ",".join(list(itemset))]))
+        
+    print(f"Generated CARs [Class: {label}]: {generated_cars}")
+        #print(f"{upper_support}: {itemset}")
+    #     for i in range(1 , len(itemset)):
+    #         # Generate possible combination of itemsets (Subset of frequent itemset is frequet itemset)
+    #         for antecedent in itertools.combinations(itemset , i):
+    #             antecedent = tuple(sorted(antecedent))
+    #             consequent = tuple(sorted(set(itemset) - set(antecedent)))
+                
+    #             # obtaining the rules where only the consequent has the class label
+    #             if len(consequent) == 1 and consequent[0] in class_label:
+    #                 if antecedent in patterns:
+    #                     lower_support = patterns[antecedent]
+    #                     confidence = float(upper_support) / lower_support
+    #                     support = upper_support/len(df)
 
-# print("Generate CARs")
-# CARs = {}
-# output = []
-    
-# for itemset in patterns.keys():
-#     upper_support = patterns[itemset]
-    
-#     for i in range(1 , len(itemset)):
-#         # Generate possible combination of itemsets (Subset of frequent itemset is frequet itemset)
-#         for antecedent in itertools.combinations(itemset , i):
-#             antecedent = tuple(sorted(antecedent))
-#             consequent = tuple(sorted(set(itemset) - set(antecedent)))
-            
-#             # obtaining the rules where only the consequent has the class label
-#             if len(consequent) == 1 and consequent[0] in class_label:
-#                 if antecedent in patterns:
-#                     lower_support = patterns[antecedent]
-#                     confidence = float(upper_support) / lower_support
-#                     support = upper_support/len(df)
+    #                     # filtering the rules where the confidence of the rules does not satisfy the minimum threshold
+    #                     if confidence >= 0.3:
+    #                         CARs[antecedent] = (consequent, confidence, support)
+    #                         output.append("\t".join([str(consequent[0]) , str(confidence) , str(support) , ",".join(list(antecedent))]))
 
-#                     # filtering the rules where the confidence of the rules does not satisfy the minimum threshold
-#                     if confidence >= 0.3:
-#                         CARs[antecedent] = (consequent, confidence, support)
-#                         output.append("\t".join([str(consequent[0]) , str(confidence) , str(support) , ",".join(list(antecedent))]))
+    # print(f"Generated CARs length: {len(CARs)}")               
+    # with open("AC_rules.tsv" , 'w') as ac_file:
+    #     ac_file.write("\n".join(output))
 
-# print(f"Generated CARs length: {len(CARs)}")               
-# with open("AC_rules.tsv" , 'w') as ac_file:
-#     ac_file.write("\n".join(output))
+
+with open("AC_rules.tsv" , 'w') as ac_file:
+    ac_file.write("\n".join(output))
 
