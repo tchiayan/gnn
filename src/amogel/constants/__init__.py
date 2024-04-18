f40708dd202791bec044e318064323fba778ecd4
TCY <tchiayan@gmail.com>
2024-04-18 22:57:47 +0800

    Update requirement and path constants

--- src/amogel/constants/__init__.py
+++ src/amogel/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAM_FILE_PATH = Path("params.yaml")
+CONFIG_FILE_PATH = Path(r"./config/config.yaml")
+PARAM_FILE_PATH = Path(r"./params.yaml")
