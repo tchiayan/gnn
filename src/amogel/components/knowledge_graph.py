51b3dc6d73120ddfab20369799f1e5d7a4863952
Tan Chia Yan <tchiayan@Tans-Laptop.local>
2024-05-29 20:30:04 +0800

    Fix bugs

--- src/amogel/components/knowledge_graph.py
+++ src/amogel/components/knowledge_graph.py
@@ -1032,8 +1032,8 @@ class KnowledgeGraph():
             for idx , sample in self.train_data.iterrows():
                 #sample_value = self.kbin_model.transform(sample.values.reshape(1, -1))[0]
                 if self.config.discretized:
-                    torch_sample = torch.tensor(sample.values, dtype=torch.int , device=device).unsqueeze(-1) # shape => number_of_node , 1 (gene expression)
-                    torch_sample = one_hot(torch_sample , num_classes=self.config.n_bins).squeeze(1)
+                    torch_sample = torch.tensor(sample.values, dtype=torch.long , device=device).unsqueeze(-1) # shape => number_of_node , 1 (gene expression)
+                    torch_sample = one_hot(torch_sample.long(), num_classes=self.config.n_bins).squeeze(1).to(device)
                 else:
                     torch_sample = torch.tensor(sample.values, dtype=torch.float32 , device=device).unsqueeze(-1) # shape => number_of_node , 1 (gene expression)
                 
