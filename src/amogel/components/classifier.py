d1de0629e48b4efe39ffb1dbef72394a99df73c7
Tan Chia Yan <tchiayan@gmail.com>
2024-07-02 21:20:30 +0800

    Scale edge matrix

--- src/amogel/components/classifier.py
+++ src/amogel/components/classifier.py
@@ -349,6 +349,9 @@ class OtherClassifier:
         
         edge_matrix = torch.stack(edge_matrix , dim=-1)
         
+        # scale the edge matrix to 0-1
+        edge_matrix = (edge_matrix - edge_matrix.min()) / (edge_matrix.max() - edge_matrix.min())
+        
         logger.info(f"Generating graph data for training...")
         train_graph = []
         
